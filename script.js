580413e1b9e1b0986d83c039621d4ab2b91b327f
Moubarak Ali-Kparah <alikparahmoubarak@gmail.com>
2025-10-27 14:38:06 -0500

    Refactor scroll event listener and navbar interactions

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.addEventListener("click", () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-});
-
-// scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.addEventListener("scroll", () => {
+window.addEventListener("scroll", function() { // use function() to bind this to window
     let top = window.scrollY;
 
     sections.forEach(sec => {
@@ -19,31 +6,28 @@ window.addEventListener("scroll", () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        // Highlight navbar links
+        // active navbar links
         if (top >= offset && top < offset + height) {
             navLinks.forEach(link => link.classList.remove('active'));
-            document.querySelector(`header nav a[href*=${id}]`).classList.add('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
 
-            // Animate section
+            // active sections for animation on scroll
             sec.classList.add('show-animate');
         } else {
-            // Remove animation if you want it to repeat
+            // remove animation if you want it to repeat
             sec.classList.remove('show-animate');
         }
     });
 
-    // Sticky navbar
+    // sticky navbar
     let header = document.querySelector('header');
-    header.classList.toggle('sticky', top > 100);
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-    // Remove toggle icon and navbar on scroll
+    // remove toggle icon and navbar when clicking navbar links (scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
-    // Footer animation
+    // footer animation on scroll
     let footer = document.querySelector('footer');
-    footer.classList.toggle(
-        'show-animate',
-        window.innerHeight + window.scrollY >= document.scrollingElement.scrollHeight
-    );
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 });
