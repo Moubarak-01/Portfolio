e96cc5e9995a5a8742b63d5f0e27bc746ce7c14e
Moubarak Ali-Kparah <alikparahmoubarak@gmail.com>
2025-10-27 14:41:42 -0500

    Refactor scroll event handling and navbar interactions

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
-window.addEventListener("scroll", function() { // use function() to bind this to window
-    let top = window.scrollY;
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.addEventListener("click", () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+});
+
+window.onscroll = () => {
     sections.forEach(sec => {
+        let top = window.scrollY;
         let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        // active navbar links
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => link.classList.remove('active'));
-            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
             // active sections for animation on scroll
             sec.classList.add('show-animate');
-        } else {
-            // remove animation if you want it to repeat
+        }
+        // if want to animation that repeats on scroll use this
+        else {
             sec.classList.remove('show-animate');
         }
     });
 
     // sticky navbar
     let header = document.querySelector('header');
+
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    // remove toggle icon and navbar when clicking navbar links (scroll)
+    // remove toggle icon and navbar when click navbar links (scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
-    // footer animation on scroll
+    // animation footer on scroll
     let footer = document.querySelector('footer');
+
     footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
-});
+}
